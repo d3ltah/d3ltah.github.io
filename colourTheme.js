a70669156373231aec8389e7bb4e8118503d6826
d3ltah <deltah404@gmail.com>
2023-09-02 17:08:08 +0100

    Theme cookie not saving fix

--- colourTheme.js
+++ colourTheme.js
@@ -1,22 +1,30 @@
 const ThemeButton = document.getElementById("theme-toggle");
 const ThemeNote = document.getElementById("theme-toggle-note");
 
-function toggleTheme() {
-    let sound = new Audio("./Pluck.mp3");
-    sound.volume = 0.1;
-    sound.load();
-    sound.play();
+function toggleThemeCookie() {
+    if (window.localStorage.getItem("theme") == "light") {
+        window.localStorage.setItem("theme", "dark");
+    } else {
+        window.localStorage.setItem("theme", "light");
+    }
+}
 
-    ThemeNote.innerHTML = `Turn on ${window.localStorage.getItem("theme")} mode >`;
+function updateTheme() {
     if (window.localStorage.getItem("theme") == "light") {
         document.documentElement.style.setProperty("--background", "#0f0f0f");
         document.documentElement.style.setProperty("--foreground", "#f0f0f0");
-        window.localStorage.setItem("theme", "dark");
         ThemeButton.style.transform = "scaleX(-1)";
     } else {
         document.documentElement.style.setProperty("--background", "#f0f0f0");
         document.documentElement.style.setProperty("--foreground", "#0f0f0f");
-        window.localStorage.setItem("theme", "light");
         ThemeButton.style.transform = "scaleX(1)";
     }
+    ThemeNote.innerHTML = `Turn on ${window.localStorage.getItem("theme")} mode >`;
 }
+
+function toggleTheme() {
+    toggleThemeCookie();
+    updateTheme();
+}
+
+updateTheme();
