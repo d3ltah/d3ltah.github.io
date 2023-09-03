e8996d414103f6a5bc6fa046bcd8950e2d5ff223
d3ltah <deltah404@gmail.com>
2023-09-03 20:52:24 +0100

    Small bugfixes for Spotify widget

--- activity.js
+++ activity.js
@@ -1,3 +1,31 @@
+function updateSpotifyDisplay() {
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=${key}&format=json&limit=1&user=bbqbeanburger&extended=1`)
+        .then((response) => response.json())
+        .then((json) => {
+            var track = json.recenttracks.track[0];
+            document.getElementById("spotify-title").innerHTML = track.name;
+            document.getElementById("spotify-artist").innerHTML = track.artist.name;
+            document.getElementById("spotify-img").src = track.image[3]["#text"];
+
+            if (!track.title == track.album["#text"]) {
+                document.getElementById("spotify-album").style.fontSize = "";
+                document.getElementById("spotify-album").innerHTML = "on " + track.album["#text"];
+            } else {
+                document.getElementById("spotify-album").style.fontSize = "0";
+            }
+
+            let nowPlaying = track['@attr'] ? track['@attr'].nowplaying === "true" : false;
+
+            if (nowPlaying == true) {
+                document.getElementById("spotify-live-dot").style.visibility = "visible";
+                document.getElementById("spotify-uts").innerHTML = "Now Playing";
+            } else {
+                document.getElementById("spotify-live-dot").style.visibility = "hidden";
+                document.getElementById("spotify-uts").innerHTML = timeSince(track.date.uts * 1000) + " ago";
+            }
+        });
+}
+
 var timeSince = function (date) {
     if (typeof date !== 'object') {
         date = new Date(date);
@@ -41,7 +69,7 @@ var timeSince = function (date) {
     if (interval < 10 && (intervalType == "second" || intervalType == "seconds")) {
         return "Just now";
     }
-    
+
     return interval + ' ' + intervalType;
 }
 
@@ -55,23 +83,5 @@ var key = "6789c0120d1863a44196d2880b1c39b0";
 
 var xmlHttp = new XMLHttpRequest();
 
-fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=${key}&format=json&limit=1&user=bbqbeanburger&extended=1`)
-    .then((response) => response.json())
-    .then((json) => {
-        var lastPlayed = json.recenttracks.track[0];
-        document.getElementById("spotify-title").innerHTML = lastPlayed.name;
-        document.getElementById("spotify-artist").innerHTML = lastPlayed.artist.name;
-        document.getElementById("spotify-album").innerHTML = "on " + lastPlayed.album["#text"];
-        document.getElementById("spotify-img").src = lastPlayed.image[3]["#text"];
-        
-        try {
-            if (lastPlayed["@attr"].nowplaying == "false" || lastPlayed["@attr"].nowplaying == undefined) {
-                document.getElementById("spotify-uts").innerHTML = timeSince(lastPlayed.date.uts * 1000) + " ago";
-                document.getElementById("spotify-live-dot").style.display = "none";
-            } else {
-                document.getElementById("spotify-uts").innerHTML = "Now playing";
-            }
-        } catch (TypeError) {
-            document.getElementById("spotify-live-dot").style.display = "none";
-        }
-    });
+updateSpotifyDisplay();
+setInterval(updateSpotifyDisplay, 5000);
