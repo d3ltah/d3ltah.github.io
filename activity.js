e650869dce1f7cc71d0ce0c48af226d411e9b83b
d3ltah <deltah404@gmail.com>
2023-09-07 16:51:58 +0100

    Add top artists

--- activity.js
+++ activity.js
@@ -1,91 +1,119 @@
+const SpotifyContainer = document.getElementById("spotify-container");
+const ArtistsContainer = document.getElementById("spotify-artists");
+const key = "6789c0120d1863a44196d2880b1c39b0";
+
 function updateSpotifyDisplay() {
-    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=${key}&format=json&limit=1&user=bbqbeanburger&extended=1`)
-        .then((response) => response.json())
-        .then((json) => {
-            var track = json.recenttracks.track[0];
-            document.getElementById("spotify-title").innerHTML = track.name;
-            document.getElementById("spotify-artist").innerHTML = track.artist.name;
-            document.getElementById("spotify-img").style.backgroundImage = `url(${track.image[3]["#text"]})`;
-            
-            if (track.name != track.album["#text"]) {
-                document.getElementById("spotify-album").innerHTML = "on " + track.album["#text"];
-                document.getElementById("spotify-album").style.width = "100%";
-                document.getElementById("spotify-album").style.height = "1em";
-            } else {
-                document.getElementById("spotify-album").style.width = "0";
-                document.getElementById("spotify-album").style.height = "0";
-            }
+  fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=${key}&format=json&limit=1&user=bbqbeanburger&extended=1`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      var track = json.recenttracks.track[0];
+      document.getElementById("spotify-title").innerHTML = track.name;
+      document.getElementById("spotify-artist").innerHTML = track.artist.name;
+      document.getElementById(
+        "spotify-img"
+      ).style.backgroundImage = `url(${track.image[3]["#text"]})`;
 
-            let nowPlaying = track['@attr'] ? track['@attr'].nowplaying === "true" : false;
+      if (track.name != track.album["#text"]) {
+        document.getElementById("spotify-album").innerHTML =
+          "on " + track.album["#text"];
+        document.getElementById("spotify-album").style.width = "100%";
+        document.getElementById("spotify-album").style.height = "1em";
+      } else {
+        document.getElementById("spotify-album").style.width = "0";
+        document.getElementById("spotify-album").style.height = "0";
+      }
 
-            if (nowPlaying == true) {
-                document.documentElement.style.setProperty("--spotify-live-dot-size", 1);
-                document.getElementById("spotify-uts").innerHTML = "Now Playing";
-            } else {
-                document.documentElement.style.setProperty("--spotify-live-dot-size", 0);
-                document.getElementById("spotify-uts").innerHTML = timeSince(track.date.uts * 1000) + " ago";
-            }
-        });
+      let nowPlaying = track["@attr"]
+        ? track["@attr"].nowplaying === "true"
+        : false;
+
+      if (nowPlaying == true) {
+        document.documentElement.style.setProperty(
+          "--spotify-live-dot-size",
+          1
+        );
+        document.getElementById("spotify-uts").innerHTML = "Now Playing";
+      } else {
+        document.documentElement.style.setProperty(
+          "--spotify-live-dot-size",
+          0
+        );
+        document.getElementById("spotify-uts").innerHTML =
+          timeSince(track.date.uts * 1000) + " ago";
+      }
+    });
 }
 
-//https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=rj&api_key=6789c0120d1863a44196d2880b1c39b0&format=json&limit=1&user=bbqbeanburger&extended=1
+function updateTopArtists() {
+  var artists = [];
+  fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=bbqbeanburger&api_key=${key}&format=json&limit=5`
+  )
+    .then((response) => response.json())
+    .then((json) => {
+      for (var i = 0; i < json.topartists.artist.length; i++) {
+        var artistElement = document.createElement("p");
+        artistElement.innerHTML = json.topartists.artist[i].name;
+        ArtistsContainer.appendChild(artistElement);
+      }
+        
+    });
+}
 
 var timeSince = function (date) {
-    if (typeof date !== 'object') {
-        date = new Date(date);
-    }
+  if (typeof date !== "object") {
+    date = new Date(date);
+  }
 
-    var seconds = Math.floor((new Date() - date) / 1000);
-    var intervalType;
+  var seconds = Math.floor((new Date() - date) / 1000);
+  var intervalType;
 
-    var interval = Math.floor(seconds / 31536000);
+  var interval = Math.floor(seconds / 31536000);
+  if (interval >= 1) {
+    intervalType = "year";
+  } else {
+    interval = Math.floor(seconds / 2592000);
     if (interval >= 1) {
-        intervalType = 'year';
+      intervalType = "month";
     } else {
-        interval = Math.floor(seconds / 2592000);
+      interval = Math.floor(seconds / 86400);
+      if (interval >= 1) {
+        intervalType = "day";
+      } else {
+        interval = Math.floor(seconds / 3600);
         if (interval >= 1) {
-            intervalType = 'month';
+          intervalType = "hour";
         } else {
-            interval = Math.floor(seconds / 86400);
-            if (interval >= 1) {
-                intervalType = 'day';
-            } else {
-                interval = Math.floor(seconds / 3600);
-                if (interval >= 1) {
-                    intervalType = "hour";
-                } else {
-                    interval = Math.floor(seconds / 60);
-                    if (interval >= 1) {
-                        intervalType = "minute";
-                    } else {
-                        interval = seconds;
-                        intervalType = "second";
-                    }
-                }
-            }
+          interval = Math.floor(seconds / 60);
+          if (interval >= 1) {
+            intervalType = "minute";
+          } else {
+            interval = seconds;
+            intervalType = "second";
+          }
         }
+      }
     }
+  }
 
-    if (interval > 1 || interval === 0) {
-        intervalType += 's';
-    }
+  if (interval > 1 || interval === 0) {
+    intervalType += "s";
+  }
 
-    if (interval < 10 && (intervalType == "second" || intervalType == "seconds")) {
-        return "Just now";
-    }
+  if (
+    interval < 10 &&
+    (intervalType == "second" || intervalType == "seconds")
+  ) {
+    return "Just now";
+  }
 
-    return interval + ' ' + intervalType;
-}
-
-var aDay = 24 * 60 * 60 * 1000;
-console.log(timeSince(new Date(Date.now() - aDay)));
-console.log(timeSince(new Date(Date.now() - aDay * 2)));
-
-const SpotifyContainer = document.getElementById("spotify-container");
-
-var key = "6789c0120d1863a44196d2880b1c39b0";
-
-var xmlHttp = new XMLHttpRequest();
+  return interval + " " + intervalType;
+};
 
 updateSpotifyDisplay();
+updateTopArtists();
 setInterval(updateSpotifyDisplay, 5000);
+
+// http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=bbqbeanburger&api_key=6789c0120d1863a44196d2880b1c39b0&format=json&limit=5
