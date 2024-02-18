5a79ab495a63679201e9f5dd24678aa2cc85f7c3
d3ltah <deltah404@gmail.com>
2024-02-18 07:55:32 +0000

    add spotify widget

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const lastFmUrl = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=bbqbeanburger&api_key=786ecc4a87d38105baee228090b1d19c&limit=1&extended=1&format=json";
 var birthdate = new Date("Jan 25 2008");
 var cur = new Date();
 var diff = cur - birthdate;
@@ -15,3 +16,29 @@ function updateTime() {
 
 updateTime();
 setInterval(updateTime, 1000);
+
+function updateSpotifyWidget() {
+	fetch(lastFmUrl)
+		.then(response => response.json())
+		.then(jsonString => {
+			const track = jsonString.recenttracks.track[0];
+			if ("@attr" in track) {
+				document.getElementById("track-name").innerHTML = track.name;
+				document.getElementById("artist-name").innerHTML = track.artist.name;
+				document.getElementById("album-name").innerHTML = track.album["#text"] == track.name ? "" : track.album["#text"];
+				document.getElementById("spot-album-cover").src = track.image[3]["#text"];
+				document.getElementById("spot-album-cover").style = "";
+				document.getElementById("spot-icon").style.opacity = 1;
+			} else {
+				document.getElementById("track-name").innerHTML = "the sound of silence";
+				document.getElementById("artist-name").innerHTML = "";
+				document.getElementById("album-name").innerHTML = "";
+				document.getElementById("spot-album-cover").src = "";
+				document.getElementById("spot-album-cover").style = "background-color: var(--body-colour); opacity: 0.5";
+				document.getElementById("spot-icon").style.opacity = 0;
+			}
+		});
+}
+
+updateSpotifyWidget();
+setInterval(updateSpotifyWidget, 1200);
