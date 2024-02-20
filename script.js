20631c1003c1859d897f939faf31a3d202c2c7d7
d3ltah <deltah404@gmail.com>
2024-02-20 16:09:40 +0000

    minor content and style changes

--- script.js
+++ script.js
@@ -37,3 +37,26 @@ function updateSpotifyWidget() {
 
 updateSpotifyWidget();
 setInterval(updateSpotifyWidget, 1200);
+
+const themeToggleBtn = document.getElementById("theme-toggle");
+const themeToggleIcon = themeToggleBtn.querySelector("i");
+
+function applyTheme() {
+	if (localStorage.getItem("theme") === "dark") {
+		document.documentElement.style.setProperty("--bg-colour", "#1a1a1a");
+		document.documentElement.style.setProperty("--body-colour", "#faf1f1");
+		themeToggleIcon.classList.remove("fa-mug-hot");
+		themeToggleIcon.classList.add("fa-martini-glass");
+	}
+	else {
+		document.documentElement.style.setProperty("--bg-colour", "#faf1f1");
+		document.documentElement.style.setProperty("--body-colour", "#1a1a1a");
+		themeToggleIcon.classList.remove("fa-martini-glass");
+		themeToggleIcon.classList.add("fa-mug-hot");
+	}
+}
+
+function toggleTheme() {
+	localStorage.setItem("theme", localStorage.getItem("theme") === "dark" ? "light" : "dark");
+	applyTheme();
+}
