dbb65b7eb7e8af298eb18d45bf64aaf34d43e86a
d3ltah <deltah404@gmail.com>
2024-02-23 11:12:57 +0000

    fix visitor count (fr this time)

--- script.js
+++ script.js
@@ -41,8 +41,8 @@ function updateSpotifyWidget() {
 updateSpotifyWidget();
 setInterval(updateSpotifyWidget, 1200);
 
-fetch("https://api.api-ninjas.com/v1/counter/?id=d3smellsoftangerines&hit=true", {
-	headers: { "X-Api-Key": "oMQdq+BBFVRrNcrZukVJQg==5CYS8gJyM8BES8Q7" },
+fetch("https://api.api-ninjas.com/v1/counter/?hit=true&id=d3smellsoftangerines", {
+	headers: { "X-Api-Key": "oMQdq+BBFVRrNcrZukVJQg==5CYS8gJyM8BES8Q7" }
 })
 	.then((response) => response.json())
 	.then((jsonString) => {
